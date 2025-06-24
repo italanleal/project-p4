d99acb6ea96ae48be256e7ef5cb0927674c40078
Jucesa <jc16032005@gmail.com>
2025-06-24 20:11:14 -0300

    escrevimento errado

--- api/database/neo4j.database.js
+++ api/database/neo4j.database.js
@@ -4,7 +4,7 @@ dotenv.config();
 import neo4j from 'neo4j-driver';
 
 const uri = process.env.NEO4J_URI;
-const user = process.env.NEO4J_USER;
+const user = process.env.NEO4J_USERNAME;
 const password = process.env.NEO4J_PASSWORD;
 const database = process.env.NEO4J_DATABASE;
 
