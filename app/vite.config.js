5b4769220e088e2c3cd419a326d794b389d6b18f
italan <italanleal@gmail.com>
2025-07-01 23:41:20 -0300

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -14,5 +14,11 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 8888
-  }
+  },
+   "rewrites": [
+    {
+      "source": "/(.*)",
+      "destination": "/index.html"
+    }
+  ]
 })
